dde0cae83f6f3009603d51c013c9e5aefc369bc2
leejunhyoung <zero082517@gmail.com>
2026-03-18 16:09:01 +0900

    Add : 플레이어 애니메이션, 점프 추가

--- My project (1-A)/Assets/PlayerController.cs
+++ My project (1-A)/Assets/PlayerController.cs
@@ -35,11 +35,11 @@ public class PlayerController : MonoBehaviour
         }
     }
     // Update is called once per frame
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.name == "door") 
-            Destroy(collision.gameObject); 
-    }
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //   if (collision.name == "door") 
+    //        Destroy(collision.gameObject); 
+    //}
     void Update()
     {
         
