2c36e6d536a523b5f29f97604af3d63383c5f0e7
leejunhyoung <zero082517@gmail.com>
2026-03-31 16:02:17 +0900

    [Feat] 카드 앞뒤 움직임

--- My project1-a 3d/Assets/Card.cs
+++ My project1-a 3d/Assets/Card.cs
@@ -5,6 +5,10 @@ public class Card : MonoBehaviour
 
     public float rotateY;
     public TextMeshProUGUI text;
+    public bool isClick = false;
+    private Quaternion flipRotation = Quaternion.Euler(0, 180f, 0);
+    private Quaternion originRotation = Quaternion.Euler(0, 0, 0);
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +20,31 @@ public class Card : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, rotateY, 0);
+        // && and || or
+        // 0 => 180 => -180 => 0
+
+        //0 < y < 180
+        
+        float currentY = transform.eulerAngles.y;
+
+        if(isClick)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, flipRotation, rotateY * Time.deltaTime);
+            //if (currentY >= 0 && currentY < 180)
+            //{
+            //    transform.Rotate(0, rotateY, 0);
+            //}
+        }
+        else
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, originRotation, rotateY * Time.deltaTime);
+        }
+
+        
+        
+    }
+    public void ClickCard()
+    {
+        isClick = !isClick;
     }
 }
